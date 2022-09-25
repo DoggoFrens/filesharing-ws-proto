c4ee31925c8be645b631f3b2c9f24ab049e8536f
Matteo Paramatti <paramattimatteo@gmail.com>
2022-09-25 13:55:20 +0100

    Export FileInfoRequestMessage from main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ export { Message } from './Message';
 export { AckMessage } from './AckMessage';
 export { FileInfoMessage } from './FileInfoMessage';
 export { FileChunkMessage } from './FileChunkMessage';
+export { FileInfoRequestMessage } from './FileInfoRequestMessage';
